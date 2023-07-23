7c5c3b189b58e30fe4454cc4fda81bc36720578b
mikhailtrunenkov <94622394+mikhailtrunenkov@users.noreply.github.com>
2023-07-23 13:14:25 +0300

    File changed

--- test_product_page.py
+++ test_product_page.py
@@ -7,17 +7,18 @@ import pytest
 #     page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
 #     page.open_product_page()           # открываем страницу
 #     page.add_to_cart()                 # Добавляем товар
-    
+
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
     
+@pytest.mark.xfail
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.go_to_login_page()
 
     
